350d48069d434fa39ade501ce9da37b72303c653
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-08-19 13:45:16 -0500

    MVP

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
-server.use("/api/users", restricted, checkRole("admin"), usersRouter);
+server.use("/api/users", restricted, usersRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
